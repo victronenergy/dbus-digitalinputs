dc3118e938a759365f3a322f6ddbb36f8bd5f2fc
Izak Burger <isburger@gmail.com>
2017-04-25 00:07:19 +0200

    Support multiple inputs in different threads.

--- dbus_pulsecount.py
+++ dbus_pulsecount.py
@@ -14,8 +14,7 @@ SAVEINTERVAL = 60000
 def pulses(path):
     from select import epoll, EPOLLPRI, EPOLLERR
 
-    while os.path.islink(path):
-        path = os.path.realpath(os.readlink(path))
+    path = os.path.realpath(path)
 
     # Set up gpio for rising edge interrupts
     with open(os.path.join(os.path.dirname(path), 'edge'), 'ab') as fp:
@@ -35,40 +34,47 @@ def pulses(path):
 def main():
     DBusGMainLoop(set_as_default=True)
     dbusservice = VeDbusService('com.victronenergy.pulsecount')
-    dbusservice.add_path('/Count', value=0)
-    dbusservice.add_path('/Frequency', value=0)
 
+    inputs = ['digital_input_{}'.format(i) for i in range(1, 3)]
 
     # Interface to settings, to store pulse count
     supported_settings = {
-        'pulsecount': ['/Settings/PulseCount/0/Count', 0, 0, MAXCOUNT]
+        inp: ['/Settings/PulseCount/{}/Count'.format(inp), 0, 0, MAXCOUNT] for inp in inputs
     }
-    settings = SettingsDevice(dbusservice.dbusconn, supported_settings, lambda *args: None, timeout=10)
-    dbusservice['/Count'] = settings['pulsecount']
+    counts = SettingsDevice(dbusservice.dbusconn, supported_settings, lambda *args: None, timeout=10)
 
-    def poll():
+    def poll(gpio):
         from time import time
+        path = os.path.join('/dev/gpio', gpio)
         stamps = [0] * 5
         idx = 0
-        for _ in pulses('/dev/gpio/digital_input_1'):
-            dbusservice['/Count'] = (dbusservice['/Count']+1) % MAXCOUNT
+        for _ in pulses(path):
+            countpath = '/{}/Count'.format(gpio)
+            dbusservice[countpath] = (dbusservice[countpath]+1) % MAXCOUNT
 
             now = time()
             stamps[idx] = now
             idx = (idx+1) % len(stamps)
 
-            dbusservice['/Frequency'] = round((len(stamps)-1)/(now-min(stamps)), 2)
+            dbusservice['/{}/Frequency'.format(gpio)] = round((len(stamps)-1)/(now-min(stamps)), 2)
 
-    # Need to run the gpio polling in separate thread. This will be done
+    # Need to run the gpio polling in separate threads. This will be done
     # using epoll(), so it will be very efficient.
     gobject.threads_init()
-    poller = Thread(target=poll)
-    poller.daemon = True
-    poller.start()
+
+    for inp in inputs:
+        dbusservice.add_path('/{}/Count'.format(inp), value=0)
+        dbusservice.add_path('/{}/Frequency'.format(inp), value=0)
+        dbusservice['/{}/Count'.format(inp)] = counts[inp]
+
+        poller = Thread(target=lambda: poll(inp))
+        poller.daemon = True
+        poller.start()
 
     # Periodically save the counter
     def save_counter():
-        settings['pulsecount'] = dbusservice['/Count']
+        for inp in inputs:
+            counts[inp] = dbusservice['/{}/Count'.format(inp)]
         gobject.timeout_add(SAVEINTERVAL, save_counter)
     gobject.timeout_add(SAVEINTERVAL, save_counter)
 
@@ -80,7 +86,8 @@ def main():
     except KeyboardInterrupt:
         pass
     finally:
-        save_counter()
+        for inp in inputs:
+            counts[inp] = dbusservice['/{}/Count'.format(inp)]
 
 if __name__ == "__main__":
     main()
