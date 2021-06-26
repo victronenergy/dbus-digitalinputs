9bfa9e59c1814f17e6cbe314383b3ad3f857ef90
Jeroen Hofstee <jhofstee@victronenergy.com>
2021-06-26 13:23:47 +0200

    v0.13

--- dbus_digitalinputs.py
+++ dbus_digitalinputs.py
@@ -16,7 +16,7 @@ from gi.repository import GLib
 from vedbus import VeDbusService
 from settingsdevice import SettingsDevice
 
-VERSION = '0.12'
+VERSION = '0.13'
 MAXCOUNT = 2**31-1
 SAVEINTERVAL = 60000
 
