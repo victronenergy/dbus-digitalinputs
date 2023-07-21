e905f2ffab92f05d0393163cd24eb0e56afee58b
Izak Burger <isburger@gmail.com>
2023-07-21 14:34:50 +0200

    v0.22

--- dbus_digitalinputs.py
+++ dbus_digitalinputs.py
@@ -16,7 +16,7 @@ from gi.repository import GLib
 from vedbus import VeDbusService, VeDbusItemImport
 from settingsdevice import SettingsDevice
 
-VERSION = '0.21'
+VERSION = '0.22'
 MAXCOUNT = 2**31-1
 SAVEINTERVAL = 60000
 
