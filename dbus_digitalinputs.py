a05bc7070af2444ec9e203f6190cd67270531f56
Izak Burger <iburger@victronenergy.com>
2025-05-22 11:39:32 +0200

    v0.28

--- dbus_digitalinputs.py
+++ dbus_digitalinputs.py
@@ -16,7 +16,7 @@ from gi.repository import GLib
 from vedbus import VeDbusService, VeDbusItemImport
 from settingsdevice import SettingsDevice
 
-VERSION = '0.27'
+VERSION = '0.28'
 MAXCOUNT = 2**31-1
 SAVEINTERVAL = 60000
 
